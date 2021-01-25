058ada7793b05f693e2261303e931a835f1ff9e2
katplee <62573178+katplee@users.noreply.github.com>
2021-01-26 03:39:26 +0900

    Basic player movement addition
    Polygon mesh generation was started. It's quite complicated.

---
+++ Blackthornprod Game Jam #3/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Runtime.InteropServices;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRb = null;
+    [SerializeField] private float moveRate = 1f;
+
+    private void OnMouseEnter()
+    {
+        Move(Mouse.current.position.ReadValue());
+    }
+
+    private void OnMouseExit()
+    {
+        StopMove();
+    }
+
+    private void StopMove()
+    {
+        playerRb.velocity = Vector2.zero;
+    }
+
+    private void Move(Vector2 mouseTouch)
+    {
+        Vector2 mouseTouchWorld = Camera.main.ScreenToWorldPoint(mouseTouch);
+
+        Vector2 moveAmount = (Vector2)transform.position - mouseTouchWorld;
+
+        playerRb.velocity = moveAmount * moveRate;
+        
+        Debug.Log($"x: {moveAmount.x}, y: {moveAmount.y}");                
+    }
+    
+}
